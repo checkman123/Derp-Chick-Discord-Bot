ef5059c742d3446a6a492b107976ad86116d9836
checkman123 <checkman3335@gmail.com>
2020-07-27 22:54:49 +0700

    change to main github repo

--- commands/github.js
+++ commands/github.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'github',
     description: "My github repo for this bot",
     execute(message,args){
-        message.channel.send('https://github.com/checkman123?tab=repositories');
+        message.channel.send('https://github.com/checkman123/Derp-Chick-Discord-Bot');
     }
 }
